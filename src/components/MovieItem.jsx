f5fe0f30b54b48c4b9918ac49b07e87ca2cb98ba
burakkgolcuk <burakkgolcuk@gmail.com>
2025-09-06 10:40:27 +0300

    Update MovieItem.jsx

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,7 +1,10 @@
+// src/components/MovieItem.jsx
+import { asset } from '../utils/asset';
+
 function MovieItem({ item }) {
   return (
     <div className="item">
-      <img src={`src/assets/${item.img}`} alt="" />
+      <img src={asset(item.img)} alt={item.name || ''} />
       <div className="movie-item-info d-flex">
         <span>{item.duration}</span>
         <a href="">Play Now</a>
