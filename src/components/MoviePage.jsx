f8b744c1cd40bf6d9fc5d926888c4efaa43d9fb3
burakkgolcuk <burakkgolcuk@gmail.com>
2025-09-06 10:40:42 +0300

    Update MoviePage.jsx

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -1,23 +1,23 @@
+// src/components/MoviePage.jsx
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
 import './MoviePage.css';
 import CommentForm from './CommentForm';
+import { asset } from '../utils/asset';
 
-function MoviePage(props) {
-  const { filmler } = props;
+function MoviePage({ filmler }) {
   const { id } = useParams();
-
-  const [film, setFilm] = useState({});
+  const [film, setFilm] = useState(null);
 
   useEffect(() => {
-    const finded = filmler.find((item) => item.id === Number(id));
-    setFilm(finded);
-  }, [id]);
+    setFilm(filmler.find((item) => item.id === Number(id)) || null);
+  }, [id, filmler]);
+
+  if (!film) return null;
 
   return (
     <div className="movie-detail">
-      <img src={`/src/assets/${film.img}`} alt="" />
+      <img src={asset(film.img)} alt={film.name} />
       <h1>{film.name}</h1>
       <div className="comments">
         <CommentForm />
